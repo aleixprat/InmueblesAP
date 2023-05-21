ebd680b66d9028b3ffc03b5dc88962dd8abc7fb6
AleixPrueba <aleix.prat4@gmail.com>
2023-05-21 15:52:40 +0200

    aviso url /api

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,13 @@ const router= require('express').Router();
 
 router.use('/inmuebles',require('./api/inmuebles'))
 
+//Aviso por si solo se pone api
+router.get('/', (req, res) => {
+  //res.render('index', { title: 'Express' });
+  console.log("hello");
+  res.json({
+    message: "Querias decir? http://localhost:3000/api/inmuebles"
+  });
+});
+
 module.exports = router;
