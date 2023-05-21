a0ecbdf9f42eb3339ac9c01e0194a2a577189fdf
AleixPrueba <aleix.prat4@gmail.com>
2023-05-21 15:38:05 +0200

    DELETE y verificaciones

--- routes/api/inmuebles.js
+++ routes/api/inmuebles.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Inmueble = require('../../models/inmueble.model');
 
+//Consultar todos los inmuebles
 router.get("/", async (req,res) => {
     try {
         const allInmuebles = await Inmueble.find();
@@ -10,6 +11,7 @@ router.get("/", async (req,res) => {
     }
 });
 
+//Creación de un inmueble
 router.post("/", async (req,res) => {
     try {
         const createdInmueble = await Inmueble.create(req.body);
@@ -17,17 +19,33 @@ router.post("/", async (req,res) => {
     } catch (err) {
         res.json({ fatal: err.message });
     }
-})
+});
 
-router.put("/inmuebleID", async (req,res) => {
+//Actualziar por ID de inmbueble
+router.put("/:inmuebleID", async (req,res) => {
     const inmuebleID = req.params.inmuebleID;
-    console.log(inmuebleID);
     try {
-        const updatedInmueble = await Inmueble.findByIdAndUpdate(inmuebleID);
+        const updatedInmueble = await Inmueble.findByIdAndUpdate(inmuebleID,  req.body, { new: true });
         res.json(updatedInmueble);
     } catch (err) {
         res.json({ fatal: err.message });
     }
-})
+});
 
+//Borrar un inmueble por su ID
+router.delete("/:inmuebleID", async (req,res) => {
+    const inmuebleID = req.params.inmuebleID;
+    try {
+        const deletedInmueble = await Inmueble.findByIdAndDelete(inmuebleID);
+        if (!deletedInmueble) {
+            return res.json({ fatal: `El inmueblecon el id "${inmuebleID}" no existe` });
+        }
+        res.json({
+            _id : inmuebleID,
+            message: "Se ha borrado correctamente"
+        });
+    } catch (err) {
+        res.json({ fatal: err.message });
+    }
+});
 module.exports = router;
